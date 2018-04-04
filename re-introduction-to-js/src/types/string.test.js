77af1813b0a4ef29922a75a765a9e9dcf11cc62e
kei2100 <kei.arima.2100@gmail.com>
2018-04-04 23:43:08 +0900

    mod

--- re-introduction-to-js/src/types/string.test.js
+++ re-introduction-to-js/src/types/string.test.js
@@ -2,3 +2,11 @@ test('concat', () => {
   // 文字列を数字 (や他の値) に足すと、すべてのものはまず最初に文字列に変換されます
   expect('1' + 2 + 3).toBe('123');
 });
+
+test('template literal', () => {
+  s = `In JavaScript \n is a line-feed.`;
+  want = `In JavaScript 
+ is a line-feed.`;
+
+  expect(s).toBe(want);
+});
