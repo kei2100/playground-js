83a912ca5de938a69c9af6d156ecf398001b6795
kei2100 <kei.arima.2100@gmail.com>
2018-04-08 22:04:26 +0900

    mod

--- re-introduction-to-js/src/types/object.test.js
+++ re-introduction-to-js/src/types/object.test.js
@@ -36,3 +36,30 @@ test('computed property names', () => {
   };
   expect(o.foo).toBe('bar');
 });
+
+test('property traverse', () => {
+  const obj = {
+    '1': 1,
+    '2': 2
+  };
+  for (const k in obj) {
+    expect(obj[k]).toBe(parseInt(obj[k]))
+  }
+
+  Object.keys(obj).forEach(k => { expect(obj[k]).toBe(parseInt(obj[k])) });
+
+  // getOwnPropertyNamesは「列挙不可能な」プロパティもまとめて返す
+  const arr = ['a', 'b'];
+  expect(Object.keys(arr)).toEqual(['0', '1']);
+  expect(Object.getOwnPropertyNames(arr).sort()).toEqual(['0', '1', 'length'])
+
+  // 列挙不可能なプロパティ
+  const obj2 = Object.create({}, {
+    getOne: {
+      value: () => { return 1 },
+      enumerable: false
+    }
+  });
+  expect(Object.keys(obj2)).toEqual([]);
+  expect(Object.getOwnPropertyNames(obj2)).toEqual(["getOne"]);
+});
