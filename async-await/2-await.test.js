2ca5df86bacdd0fcd58aae7614f6f01fcae6fe4d
kei2100 <kei.arima.2100@gmail.com>
2018-05-24 20:50:06 +0900

    add async with for

--- async-await/2-await.test.js
+++ async-await/2-await.test.js
@@ -4,7 +4,7 @@
  * awaitは、async functionの中でしか使用できない
  */
 
-test('await', () => {
+test('await', async () => {
   const f1 = () => {
     return new Promise(resolve => {
       setTimeout(resolve.bind(undefined, 'f1'), 10);
@@ -17,10 +17,26 @@ test('await', () => {
 
   const f3 = async function () {
     const v1 = await f1();
-    const v2 =await f2();
+    const v2 = await f2();
     return v1 + v2;
   };
 
-  f3().then(v => { expect(v).toBe('f1f2'); })
+  await expect(f3()).resolves.toBe('f1f2');
+});
+
+test('await with for', async () => {
+  const f1 = async function () {
+    return 1;
+  };
+
+  const f2 = async function () {
+    let v = 0;
+    for (let i = 0; i < 3; i++) {
+      v += await f1();
+    }
+    return v;
+  };
+
+  await expect(f2()).resolves.toBe(3);
 });
 
